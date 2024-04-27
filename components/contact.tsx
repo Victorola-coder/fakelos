428e09974af0f63a96509851b5c49fcf1cd22dcc
Victor Olatunji <victoluolatunji@gmail.com>
2024-04-27 10:22:24 +0100

    contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -28,12 +28,12 @@ const Contact = () => {
     <section className="mx-[30px] mt-[60px] lg:mx-[76px] lg:mt-[150px]">
       <div className="flex flex-col items-center gap-[35px] md:gap-[25px]">
         <h3
-          className={`font-medium text-[#FFFFFF99] text-lg text-center max-w-[320px] md:max-w-[992px] md:text-[25px] mmd:leading-[37.5px]`}
+          className={`font-medium text-[#FFFFFF99] text-lg text-center max-w-[320px] md:max-w-[992px] md:text-[25px] md:leading-[37.5px]`}
         >
           I can help you design, improve or build the product experience for
           your new or existing products. Feel free to get in touch with me.
         </h3>
-        <h1 className="font-clashbold font-semibold text-xl text-white md:text-[60px] md:leading-[73.8px]">
+        <h1 className="font-clashbold_ font-semibold text-xl text-white md:text-[60px] md:leading-[73.8px]">
           Do You Have Any Ideas?
         </h1>
 
